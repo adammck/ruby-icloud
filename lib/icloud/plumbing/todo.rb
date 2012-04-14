df30694414718f8b3ccb513f085ed536533cf3fb
adammck <adam.mckaig@gmail.com>
2012-04-14 06:03:08 -0400

    Fix fields of Todo.

--- lib/icloud/plumbing/todo.rb
+++ lib/icloud/plumbing/todo.rb
@@ -4,7 +4,11 @@
 module ICloud
   class Todo
     include Record
-    has_fields :description, :etag, :location, :title
+    has_fields :alarms, :created_by_date, :created_by_name, :created_by_name_first,
+               :created_by_name_last, :created_date, :due_date, :due_date_is_all_day,
+               :etag, :extended_details_are_included, :guid, :has_attachments,
+               :last_modified_date, :p_guid, :title, :updated_by_date, :updated_by_name,
+               :updated_by_name_first, :updated_by_name_last
 
     def to_s
       title
