f6cebb6b493e6e7ace1b7b8d7670e7a37d3a0fd4
adammck <adam.mckaig@gmail.com>
2012-04-24 17:17:54 -0400

    Reformat record fields.
    This is untidy, but beats reformating the entire section every time I
    discover a new field in the API.

--- lib/icloud/plumbing/alarm.rb
+++ lib/icloud/plumbing/alarm.rb
@@ -4,7 +4,14 @@
 module ICloud
   class Alarm
     include Record
-    has_fields :description, :guid, :is_location_based, :message_type, :on_date, :p_guid
+    has_fields(
+      :description,
+      :guid,
+      :is_location_based,
+      :message_type,
+      :on_date,
+      :p_guid
+    )
 
     def inspect
       "#<Alarm %p date=%p>" % [
--- lib/icloud/plumbing/todo.rb
+++ lib/icloud/plumbing/todo.rb
@@ -4,10 +4,27 @@
 module ICloud
   class Todo
     include Record
-    has_fields :alarms, :created_by_date, :created_by_name, :created_by_name_first,
-               :created_by_name_last, :created_date, :due_date, :due_date_is_all_day,
-               :etag, :extended_details_are_included, :guid, :has_attachments,
-               :last_modified_date, :p_guid, :title, :updated_by_date, :updated_by_name,
-               :updated_by_name_first, :updated_by_name_last
+    has_fields(
+      :alarms,
+      :created_by_date,
+      :created_by_name,
+      :created_by_name_first,
+      :created_by_name_last,
+      :created_date,
+      :description,
+      :due_date,
+      :due_date_is_all_day,
+      :etag,
+      :extended_details_are_included,
+      :guid,
+      :has_attachments,
+      :last_modified_date,
+      :p_guid,
+      :title,
+      :updated_by_date,
+      :updated_by_name,
+      :updated_by_name_first,
+      :updated_by_name_last
+    )
   end
 end
