b2083ac2f8861468cd69dd71cc585a663078c510
Adam Mckaig <adam.mckaig@gmail.com>
2012-11-28 22:41:27 -0500

    Expire VCR casettes weekly

--- test/test_helpers.rb
+++ test/test_helpers.rb
@@ -3,11 +3,13 @@
 
 require "vcr"
 
+# Totally arbitrary GUID for this library
 TEST_CLIENT_ID = "61384E90-4A30-4F2A-89C3-E031E77B4B78"
 
 VCR.configure do |c|
   here = File.dirname(__FILE__)
-  c.allow_http_connections_when_no_cassette = true
+  c.allow_http_connections_when_no_cassette = false
+  c.default_cassette_options = { :re_record_interval => (7 * 86400) }
   c.cassette_library_dir = "#{here}/fixtures/cassettes"
   c.hook_into :webmock
 end
