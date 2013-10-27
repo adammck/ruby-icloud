f5260801294b837d10e459a33a76424578949de7
Shinji Tanaka <shinji.tanaka@gmail.com>
2013-10-27 14:37:46 +0900

    working with ruby 2.0.0

--- README.md
+++ README.md
@@ -75,6 +75,7 @@ This gem is [tested against] [travis]:
 * Ruby 1.8.7
 * Ruby 1.9.2
 * Ruby 1.9.3
+* Ruby 2.0.0
 
 
 ## License
--- lib/icloud/core_ext/date_time.rb
+++ lib/icloud/core_ext/date_time.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 # vim: et ts=2 sw=2
 
+require 'date'
+
 class DateTime
   def to_icloud
     ICloud.date_to_icloud(self)
